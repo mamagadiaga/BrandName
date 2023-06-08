4d181ca0bc3940523ac57117cb82689f49409487
Mame Gadiaga <mamsogdg@gmail.com>
2023-06-08 10:05:45 +0000

    Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import Section2 from './components/Section2';
-import Section3 from './components/Section3';
-import Section4 from './components/Section4';
-import Section5 from './components/Section5';
-import Section6 from './components/Section6';
-import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Section6 />
-      <Footer />
     </div>
   );
 }
